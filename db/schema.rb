bac4e6d186a971543f101acd9dfc9cc7743e7b96
Duncan Neal <duncan.neal@gmail.com>
2015-06-28 19:14:48 -0400

    update

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150628043431) do
+ActiveRecord::Schema.define(version: 20150628204902) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "total"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "payola_affiliates", force: :cascade do |t|
     t.string   "code"
