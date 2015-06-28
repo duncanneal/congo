29581ae7a2e5d0709d1395d55b6c9d6e44906319
Duncan Neal <duncan.neal@gmail.com>
2015-06-28 15:38:59 -0400

    Image load attempt

--- db/migrate/20150626193854_create_payola_models.rb
+++ db/migrate/20150626193854_create_payola_models.rb
@@ -1,9 +1,9 @@
 class CreatePayolaModels < ActiveRecord::Migration
   def change
     create_table :payola_models do |t|
-      t.integer :price
-      t.string :name
-      t.string :permalink
+      t.integer  :price
+      t.string   :name
+      t.string   :permalink
 
       t.timestamps null: false
     end
