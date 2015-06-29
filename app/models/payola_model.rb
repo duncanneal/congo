a275c8fe4fe5c85be1bfc3233b177b42d1b2ca7f
Derek Dyer <derek@digitalderek.com>
2015-06-29 17:22:44 -0400

    Prices too low on items caused a 404 error with Payola

--- app/models/payola_model.rb
+++ app/models/payola_model.rb
@@ -1,4 +1,12 @@
 class PayolaModel < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+  
+  validates :name, uniqueness: true
+
   include Payola::Sellable
+
+  def redirect_path(_sale)
+    '/'
+    #Rails.application.routes.url_helpers.receipt_cart_url(permalink)
+  end
 end
